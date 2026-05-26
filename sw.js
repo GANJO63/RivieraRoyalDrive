d311c98374be7210e2a17750ba41b22177310298
Cobrapaladin <142548455+GANJO63@users.noreply.github.com>
2026-05-26 11:59:21 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'rrd-v1';
+const FILES = ['/', '/index.html', '/admin.html', '/manifest.json', '/icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
